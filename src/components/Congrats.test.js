05b3c211fb2d437fb4c18e0b0365ea56e7ecd71b
Modestas <modestask97@gmail.com>
2020-01-10 13:18:14 +0000

    boilerplate tests

--- src/components/Congrats.test.js
+++ src/components/Congrats.test.js
@@ -4,3 +4,9 @@ import EnzymeAdapter from "enzyme-adapter-react-16";
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adpater: new EnzymeAdapter() });
+
+test("renders without crashing", () => {});
+
+test("renders no text when 'success' prop is false", () => {});
+
+test("renders non-empty congrats message when 'success' prop is true", () => {});
