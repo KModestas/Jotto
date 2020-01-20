b9fa7bdb9ae0f260bd23e1220c7d33f2a83e1f5e
Modestas <modestask97@gmail.com>
2020-01-20 18:02:41 +0000

    guessedWord component

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,7 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const GuessedWords = props => <div></div>;
+const GuessedWords = props => (
+  <div data-test="component-guessed-words">
+    <span data-test="guess-instructions">
+      {props.guessedWords.length && "Try to guess the word!"}
+    </span>
+  </div>
+);
 
 // .shape is used to describe an object whose keys may be of different types
 // .objectOf is used for objects that have keys of the the same type
