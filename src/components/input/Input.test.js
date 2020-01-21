4d113a533706530ea01445c855e3292cf6b7c8e9
Modestas <modestask97@gmail.com>
2020-01-21 16:21:50 +0000

    comments

--- src/components/input/Input.test.js
+++ src/components/input/Input.test.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import { findByTestAttr } from "../../../Test/testUtils";
+import { findByTestAttr, storeFactory } from "../../../Test/testUtils";
 import Input from "./Input";
+
+// .dive() goes deeper into the child components of shallow created virtual DOM
+// you can chain these to go one layer deeper
+// without dive in this case, you will see Input wrapped in redux's HOC (provided by connect())
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  // add test store as a prop to test component (mimicks provider in index.js which makes store avaliable to all connected components)
+  const wrapper = shallow(<Input store={store} />)
+    .dive()
+    .dive();
+  return wrapper;
+};
+// console.log(wrapper.debug());
+
+setup();
 
 describe("render ", () => {
   describe("word has not been guessed", () => {
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,13 @@
 import checkPropTypes from "check-prop-types";
 // checkPropTypes returns  errors from propTypes rather than just console logging them
+import { createStore } from "redux";
+
+import rootReducer from "../src/redux/reducers";
+
+// creates a testing store using actual reducers in the app
+export const storeFactory = initialState => {
+  return createStore(rootReducer, initialState);
+};
 
 export const findByTestAttr = (wrapper, val) =>
   wrapper.find(`[data-test="${val}"]`);
