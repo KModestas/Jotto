3e0efba34609199a6762a877ed370fc68e0e61dd
Modestas <modestask97@gmail.com>
2020-01-21 14:22:31 +0000

    correctGuess action

---
+++ src/redux/actions/index.js
@@ -0,0 +1,9 @@
+export const types = {
+  CORRECT_GUESS: "CORRECT_GUESS"
+};
+
+export const correctGuess = () => {
+  return {
+    type: types.CORRECT_GUESS
+  };
+};
---
+++ src/redux/actions/index.test.js
@@ -0,0 +1,12 @@
+import { correctGuess, types } from "./";
+
+describe("correctGuess", () => {
+  test('should return action with type "CORRECT_GUESS"', () => {
+    const action = correctGuess();
+    expect(action).toEqual({ type: types.CORRECT_GUESS });
+  });
+});
+
+// strings are immutable, all methods return a new string
+// arrays and objects are mutable dataTypes, therefore using toBe or === doesn't work. You need to use toEqual which compares the contents
+// toEqual is deep => compares nested properties and values
