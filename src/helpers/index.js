b4dde7f5909e8ff6b6e718d81ee4184cadd64f4f
Modestas <modestask97@gmail.com>
2020-01-21 12:04:52 +0000

    getLetterMatchCount comment

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,14 @@
 export const getLetterMatchCount = (guessedWord, secretWord) => {
   const secretLetterSet = new Set(secretWord.split(""));
+  // => { 'p', 'a', 'r', 't', 'y' }
   const guessedLetterSet = new Set(guessedWord.split(""));
+  // "parka" => { 'p', 'a', 'r', 'k' }  - guessedLetterSet will automatically filter out duplicate keys
   return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter))
     .length;
+  // [...secretLetterSet] => [ 'p', 'a', 'r', 't', 'y' ]
 };
+
+// sets always contain with unique values
+// you can chain .add() method because it returns the set itself
+// if you try to add a duplicate, it will just return the set without the added duplicate
+// .has returns true or false if set contains the element you are checking
