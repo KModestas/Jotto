e57d6ae737594cb5512afef78207cda783a9ea96
Modestas <modestask97@gmail.com>
2020-01-21 10:58:03 +0000

    app.css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import Congrats from "./Congrats";
+import GuessedWords from "./GuessedWords";
 
-function App() {
-  return (
-    <div className="App">
-      <Congrats />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Jotto</h1>
+        <Congrats success={false} />
+        <GuessedWords guessedWords={[]} />
+      </div>
+    );
+  }
 }
 
 export default App;
