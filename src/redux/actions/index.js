922863e5019562ba1ce2c7dba4396db228eb2241
Modestas <modestask97@gmail.com>
2020-01-22 11:20:53 +0000

    guessedwords reducer

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,10 @@
 export const actionTypes = {
-  CORRECT_GUESS: "CORRECT_GUESS"
+  CORRECT_GUESS: "CORRECT_GUESS",
+  GUESS_WORD: "GUESS_WORD"
+};
+
+export const guessWord = guessedWord => {
+  return (dispatch, getState) => {};
 };
 
 export const correctGuess = () => {
---
+++ src/test/integration.test.js
@@ -0,0 +1,15 @@
+import { storeFactory } from "./testUtils";
+import { guessWord } from "../redux/actions";
+
+describe("guessWord action dispatcher", () => {
+  describe("no guessed words", () => {
+    test("updates state correctly for wrong guess ", () => {});
+
+    test("updates state correctly for right guess ", () => {});
+  });
+
+  describe("some guessed words", () => {
+    test("updates state correctly for wrong guess ", () => {});
+    test("updates state correctly for right guess  ", () => {});
+  });
+});
