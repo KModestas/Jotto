0dd0178099a170427ec79e635d8c363ab2de9121
Modestas <modestask97@gmail.com>
2020-01-22 14:06:51 +0000

    getSecretWord

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,9 @@
+import axios from "axios";
 import { getLetterMatchCount } from "../../helpers";
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
-  GUESS_WORD: "GUESS_WORD"
+  GUESS_WORD: "GUESS_WORD",
+  SET_SECRET_WORD: "SET_SECRET_WORD"
 };
 
 export const guessWord = guessedWord => {
@@ -21,5 +23,11 @@ export const guessWord = guessedWord => {
 export const correctGuess = () => {
   return {
     type: actionTypes.CORRECT_GUESS
+  };
+};
+
+export const getSecretWord = () => {
+  return dispatch => {
+    axios.get("http://localhost:3000");
   };
 };
