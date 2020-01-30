1e911f96038d076fcf09ab138b93f056cbae73f1
Modestas <modestask97@gmail.com>
2020-01-30 15:59:43 +0000

    congrats tweak

--- src/components/Congrats.js
+++ src/components/Congrats.js
@@ -1,15 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Congrats = props => (
-  <div data-test="component-congrats" className="alert alert-success">
-    {props.success && (
-      <span data-test="congrats-message">
-        Congratulations, you guessed the word!
-      </span>
-    )}
-  </div>
-);
+const Congrats = props => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats" className="alert alert-success">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
+};
 
 Congrats.propTypes = {
   success: PropTypes.bool.isRequired
