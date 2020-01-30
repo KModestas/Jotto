00df0ccaadb246581f1db9161a2041fcc86c1beb
Modestas <modestask97@gmail.com>
2020-01-30 16:03:54 +0000

    b

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ export class UnconnectedApp extends Component {
     return (
       <div className="container">
         <h1>Jotto</h1>
-        <h3>Secret Word: {this.props.secretWord}</h3>
+        <b>Secret Word: {this.props.secretWord}</b>
         <Congrats success={this.props.success} />
         <Input />
         <GuessedWords guessedWords={this.props.guessedWords} />
