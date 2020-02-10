703734c848fbbe6b5c57db0f5cacf55e5e3a5ef6
Modestas <modestask97@gmail.com>
2020-02-10 12:43:50 +0000

    test utils

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,53 +1,48 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { guessWord } from "../redux/actions";
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { guessWord } from '../redux/actions'
 
 export class UnconnectedInput extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentGuess: ""
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+	constructor() {
+		super()
+		this.state = {
+			currentGuess: ''
+		}
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
 
-  handleSubmit(e) {
-    e.preventDefault();
-    const currentGuess = this.state.currentGuess;
-    if (currentGuess) {
-      this.props.guessWord(currentGuess);
-      this.setState({ currentGuess: "" });
-    }
-  }
+	handleSubmit(e) {
+		e.preventDefault()
+		const currentGuess = this.state.currentGuess
+		if (currentGuess) {
+			this.props.guessWord(currentGuess)
+			this.setState({ currentGuess: '' })
+		}
+	}
 
-  render() {
-    const contents = this.props.success ? null : (
-      <form className="form-inline">
-        <input
-          data-test="input-box"
-          className="mb-2 mx-sm-1"
-          type="text"
-          value={this.state.currentGuess}
-          onChange={e => this.setState({ currentGuess: e.target.value })}
-          placeholder="Enter guess ..."
-        />
-        <button
-          data-test="submit-button"
-          className="btn btn-primary mb-2"
-          type="submit"
-          onClick={this.handleSubmit}
-        >
-          SUBMIT
-        </button>
-      </form>
-    );
-    return <div data-test="component-input">{contents}</div>;
-  }
+	render() {
+		const contents = this.props.success ? null : (
+			<form className='form-inline'>
+				<input
+					data-test='input-box'
+					className='mb-2 mx-sm-1'
+					type='text'
+					value={this.state.currentGuess}
+					onChange={e => this.setState({ currentGuess: e.target.value })}
+					placeholder='Enter guess ...'
+				/>
+				<button data-test='submit-button' className='btn btn-primary mb-2' type='submit' onClick={this.handleSubmit}>
+					SUBMIT
+				</button>
+			</form>
+		)
+		return <div data-test='component-input'>{contents}</div>
+	}
 }
 
 const mapStateToProps = ({ success }) => {
-  return { success };
-};
+	return { success }
+}
 
 // passing an object as second argument instead of mapDispatchToProps func because all we need to do is pass guessWord - we dont need to the full utility
-export default connect(mapStateToProps, { guessWord })(UnconnectedInput);
+export default connect(mapStateToProps, { guessWord })(UnconnectedInput)
--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,26 +1,8 @@
-import checkPropTypes from "check-prop-types";
-// checkPropTypes returns  errors from propTypes rather than just console logging them
-import { createStore, applyMiddleware } from "redux";
+import checkPropTypes from 'check-prop-types'
 
-import rootReducer from "../redux/reducers";
-import { middleware } from "../redux/store";
-
-// creates a testing store using actual reducers in the app
-export const storeFactory = initialState => {
-  // create an instance of the store with middleware using the real rootreducer and middleware
-  const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
-  return createStoreWithMiddleware(rootReducer, initialState);
-};
-
-export const findByTestAttr = (wrapper, val) =>
-  wrapper.find(`[data-test="${val}"]`);
+export const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test="${val}"]`)
 
 export const checkProps = (component, expectedProps) => {
-  const PropError = checkPropTypes(
-    component.propTypes,
-    expectedProps,
-    "prop",
-    component.name
-  );
-  expect(PropError).toBeUndefined();
-};
+	const PropError = checkPropTypes(component.propTypes, expectedProps, 'prop', component.name)
+	expect(PropError).toBeUndefined()
+}
